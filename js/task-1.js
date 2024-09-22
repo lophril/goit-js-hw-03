fda75db236a15d0bfd968d4068f99fc12c8e27b5
lophril <ilya41290@gmail.com>
2024-09-22 17:57:48 +0300

    .

--- js/task-1.js
+++ js/task-1.js
@@ -6,4 +6,4 @@ function slugify(title) {
 console.log(slugify("Arrays for beginners"));
 console.log(slugify("English for developer"));
 console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
